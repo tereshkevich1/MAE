911c56f4331e4a13bdd27cf125f71027732f11dc
Babananick <babananick@gmail.com>
2024-02-19 02:41:18 +0300

    Added started design for RegistrationActivity

--- cab/src/main/java/com/example/cab/RegistrationActivity.kt
+++ cab/src/main/java/com/example/cab/RegistrationActivity.kt
@@ -2,10 +2,10 @@ package com.example.cab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-class MainActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registration)
     }
 }
