6e3e45ee75a0b124dc636a45d0322e612d473132
Babananick <babananick@gmail.com>
2024-02-23 16:47:27 +0300

    Linked xml to view model

--- cab/src/main/java/com/example/cab/RegistrationActivity.kt
+++ cab/src/main/java/com/example/cab/RegistrationActivity.kt
@@ -21,5 +21,7 @@ class RegistrationActivity : AppCompatActivity() {
         registrationBinding.lifecycleOwner = this
 
         setContentView(registrationView)
+
+
     }
 }
--- cab/src/main/java/com/example/cab/vm/RegistrationActivityViewModel.kt
+++ cab/src/main/java/com/example/cab/vm/RegistrationActivityViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.cab.vm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class RegistrationActivityViewModel: ViewModel() {
+    private val _name = MutableLiveData("")
+    private val _surname = MutableLiveData("")
+    private val _phone = MutableLiveData<String>()
+
+    val name: LiveData<String> = _name
+    val surname: LiveData<String> = _surname
+    val phone: LiveData<String> = _phone
+
+
+
 }
