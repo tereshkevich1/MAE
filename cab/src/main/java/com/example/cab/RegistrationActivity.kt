aebca54e969704b162b3cb4957422e69a3496970
Babananick <babananick@gmail.com>
2024-02-24 00:19:13 +0300

    Fixed not remembering phone field. Layout changes

--- cab/src/main/java/com/example/cab/RegistrationActivity.kt
+++ cab/src/main/java/com/example/cab/RegistrationActivity.kt
@@ -21,7 +21,5 @@ class RegistrationActivity : AppCompatActivity() {
         registrationBinding.lifecycleOwner = this
 
         setContentView(registrationView)
-
-
     }
 }
--- cab/src/main/java/com/example/cab/vm/RegistrationActivityViewModel.kt
+++ cab/src/main/java/com/example/cab/vm/RegistrationActivityViewModel.kt
@@ -7,12 +7,10 @@ import androidx.lifecycle.ViewModel
 class RegistrationActivityViewModel: ViewModel() {
     private val _name = MutableLiveData("")
     private val _surname = MutableLiveData("")
-    private val _phone = MutableLiveData<String>()
+    private val _phone = MutableLiveData("")
 
     val name: LiveData<String> = _name
     val surname: LiveData<String> = _surname
     val phone: LiveData<String> = _phone
-
-
 
 }
