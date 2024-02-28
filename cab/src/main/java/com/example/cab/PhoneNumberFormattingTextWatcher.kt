84431cbb3f1a1456e18946d6bd2f19e477d607d1
Babananick <babananick@gmail.com>
2024-02-28 18:20:01 +0300

    Partly fixed deletion in middle in PhoneNumberFormattingTextWatcher

--- cab/src/main/java/com/example/cab/PhoneNumberFormattingTextWatcher.kt
+++ cab/src/main/java/com/example/cab/PhoneNumberFormattingTextWatcher.kt
@@ -5,36 +5,75 @@ import android.text.TextWatcher
 import android.widget.EditText
 
 class PhoneNumberFormattingTextWatcher(private val editText: EditText) : TextWatcher {
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-        // No action needed here
+
+    private var isDeleting = false
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        isDeleting = count > after
     }
 
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        // No action needed here
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        // No operation
     }
 
     override fun afterTextChanged(s: Editable) {
+        editText.removeTextChangedListener(this)
+
         val text = s.toString()
         val resources = editText.context.resources
 
-        if (text.length == 1 && !text.startsWith("+")) {
-            editText.setText(resources.getString(R.string.phone_number_format_plus, text))
-            editText.setSelection(editText.text.length)
-        } else if (text.length > 4 && text[4] != ' ') {
-            editText.setText(resources.getString(R.string.phone_number_format_space, text.substring(0, 4), text.substring(4)))
-            editText.setSelection(editText.text.length)
-        } else if (text.length > 7 && text[7] != ' ') {
-            editText.setText(resources.getString(R.string.phone_number_format_space, text.substring(0, 7), text.substring(7)))
-            editText.setSelection(editText.text.length)
-        } else if (text.length > 11 && text[11] != ' ') {
-            editText.setText(resources.getString(R.string.phone_number_format_space, text.substring(0, 11), text.substring(11)))
-            editText.setSelection(editText.text.length)
-        } else if (text.length > 14 && text[14] != ' ') {
-            editText.setText(resources.getString(R.string.phone_number_format_space, text.substring(0, 14), text.substring(14)))
+        // Remember the original cursor position
+        var originalPosition = editText.selectionStart
+
+        // Remove non-numerical characters
+        val cleaned = StringBuilder()
+        for (char in text) {
+            if (char.isDigit() || char == '+' || char == ' ') {
+                cleaned.append(char)
+            }
+        }
+
+        val cleanedText = cleaned.toString()
+
+        if (isDeleting) {
+            editText.setText(cleanedText)
+        } else {
+            if (cleanedText.length == 1 && !cleanedText.startsWith("+")) {
+                editText.setText(resources.getString(R.string.phone_number_format_plus, cleanedText))
+                originalPosition++
+            } else if (cleanedText.length > 4 && cleanedText[4] != ' ') {
+                editText.setText(resources.getString(R.string.phone_number_format_space, cleanedText.substring(0, 4), cleanedText.substring(4)))
+                if (originalPosition > 4) originalPosition++
+            } else if (cleanedText.length > 7 && cleanedText[7] != ' ') {
+                editText.setText(resources.getString(R.string.phone_number_format_space, cleanedText.substring(0, 7), cleanedText.substring(7)))
+                if (originalPosition > 7) originalPosition++
+            } else if (cleanedText.length > 11 && cleanedText[11] != ' ') {
+                editText.setText(resources.getString(R.string.phone_number_format_space, cleanedText.substring(0, 11), cleanedText.substring(11)))
+                if (originalPosition > 11) originalPosition++
+            } else if (cleanedText.length > 14 && cleanedText[14] != ' ') {
+                editText.setText(resources.getString(R.string.phone_number_format_space, cleanedText.substring(0, 14), cleanedText.substring(14)))
+                if (originalPosition > 14) originalPosition++
+            }
+        }
+
+        // Restore the cursor position
+        if (originalPosition <= editText.text.length) {
+            editText.setSelection(originalPosition)
+        } else {
             editText.setSelection(editText.text.length)
         }
+
+        editText.addTextChangedListener(this)
     }
 }
+
+
+
+
+
+
+
+
 
 
 
