69694eec8dd71380c3c7be693f40a5e5b20739eb
Babananick <babananick@gmail.com>
2024-03-03 15:20:58 +0300

    Moved POSITIONS_FOR_SPACES and ON_ADJUSTING_POSITIONS to companion object

--- cab/src/main/java/com/example/cab/PhoneFormattingTextWatcher.kt
+++ cab/src/main/java/com/example/cab/PhoneFormattingTextWatcher.kt
@@ -6,11 +6,20 @@ import android.widget.EditText
 
 class PhoneFormattingTextWatcher(private val editText: EditText) : TextWatcher {
 
+    companion object {
+        // Positions of digits in unformatted phone, after which space have to be placed
+        private val POSITIONS_FOR_SPACES = listOf(3,5,8,10,12)
+
+        /*
+        Positions of digits right after spaces in formatted phone.
+        Using to adjust cursor position
+         */
+        private val ON_ADJUSTING_POSITIONS = listOf(5,8,12,15)
+    }
+
     private var isDeleting = false
-    private var clearedText = editText.text.toString()
+    private var unformattedPhone = editText.text.toString()
     private var position = editText.selectionStart
-    private val positionsForSpaces = listOf(3,5,8,10,12)
-    private val spacePositions = listOf(5,8,12,15)
     private val stringBuilder = StringBuilder()
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -19,32 +28,28 @@ class PhoneFormattingTextWatcher(private val editText: EditText) : TextWatcher {
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         if (s != null) {
-            clearedText = clearInput(s)
+            unformattedPhone = s.toUnformatted() as String
         }
         position = editText.selectionStart
     }
 
     override fun afterTextChanged(s: Editable) {
         editText.removeTextChangedListener(this)
-        editText.setText(createFormattedNumber(clearedText))
+        editText.setText(createFormattedNumber(unformattedPhone))
         editText.changeSelection()
         editText.addTextChangedListener(this)
     }
 
-    private fun clearInput(input: CharSequence): String {
-        stringBuilder.clear()
-        for (char in input) {
-            if (char.isDigit()) {
-                stringBuilder.append(char)
-            }
-        }
-        return stringBuilder.toString()
+    // Clears non-digit characters from the input
+    private fun CharSequence.toUnformatted(): CharSequence {
+        return this.filter { it.isDigit() }
     }
 
+    // Formats the input string as a phone number
     private fun createFormattedNumber(input: String): String {
         stringBuilder.clear().append("+")
         for ((index, digit) in input.withIndex()) {
-            if (index in positionsForSpaces) {
+            if (index in POSITIONS_FOR_SPACES) {
                 stringBuilder.append(" ")
             }
             stringBuilder.append(digit)
@@ -56,6 +61,7 @@ class PhoneFormattingTextWatcher(private val editText: EditText) : TextWatcher {
         editText.setSelection(this.coerceIn(0, editText.text.length))
     }
 
+    // Adjusts the selection position based on whether text is being deleted or added
     private fun EditText.changeSelection(){
         this.adjustSelection()
         position.setAsSelection()
@@ -66,11 +72,12 @@ class PhoneFormattingTextWatcher(private val editText: EditText) : TextWatcher {
             position++
             if (position < this.text.length) {
                 position--
-                if (position in spacePositions){
+                if (position in ON_ADJUSTING_POSITIONS){
                     position++
                 }
             }
         }
     }
 }
+
 
