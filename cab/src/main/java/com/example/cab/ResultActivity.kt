94ecaf64ff8d6d7d861e100a09f8a95a291e2343
Babananick <babananick@gmail.com>
2024-03-09 23:49:31 +0300

    Renamed started activity

--- cab/src/main/java/com/example/cab/ResultActivity.kt
+++ cab/src/main/java/com/example/cab/ResultActivity.kt
@@ -2,10 +2,10 @@ package com.example.cab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_result)
     }
 }
