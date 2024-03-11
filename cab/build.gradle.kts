c575ea86a4baec404ff3bd5ebdd3f4469d5da579
vitaliyadolgaya <92163465+vitaliyadolgaya@users.noreply.github.com>
2024-03-11 19:57:43 +0300

    UI tests for RegistrationActivity (cab)

--- cab/build.gradle.kts
+++ cab/build.gradle.kts
@@ -20,6 +20,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -53,4 +54,6 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }
